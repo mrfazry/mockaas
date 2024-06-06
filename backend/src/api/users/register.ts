fc43359f4ae161ce8e816a038b0390d5ab495222
M. R. Fazry <fazry@theravenry.com>
2024-06-06 21:31:10 +0700

    Add functionality for registering and signing in user

--- backend/src/api/users/register.ts
+++ backend/src/api/users/register.ts
@@ -1,9 +1,82 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+
+import prisma from "../../db/prisma";
+import { Prisma } from "@prisma/client";
+import { hashPassword } from "../../utils/bcrypt";
 
 const register = new Hono();
 
-register.post("", (c) => {
-  return c.json("registering the user", 201);
+const schema = z.object({
+  username: z
+    .string()
+    .trim()
+    .min(4, "Username must be at least 4 characters")
+    .max(20, "Username must be at most 20 characters")
+    .regex(
+      /^[a-zA-Z][a-zA-Z0-9]*$/gi,
+      "Username must only contain alphanumeric characters and without space"
+    ),
+  email: z.string().trim().email("Email must be valid"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
+
+register.post(
+  "",
+  zValidator("json", schema, (result, c) => {
+    if (!result.success) {
+      const err = result.error.flatten();
+
+      return c.json({ error: err.fieldErrors }, 400);
+    }
+  }),
+  async (c) => {
+    const payload = await c.req.json();
+
+    try {
+      const { hashedPassword, error } = await hashPassword(payload.password);
+
+      if (error) {
+        return c.json({ error: { password: error } }, 400);
+      }
+
+      const user = await prisma.user.create({
+        data: { ...payload, password: hashedPassword },
+      });
+    } catch (err) {
+      console.log(err);
+
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (err.code === "P2002") {
+          const fields = err.meta?.target;
+
+          let errPayload: { [k in string]: string } = {};
+
+          if (Array.isArray(fields)) {
+            fields.map((f) => {
+              errPayload[f] = `"${f}" must be unique`;
+            });
+          }
+
+          return c.json(
+            {
+              error: {
+                errPayload,
+              },
+            },
+            400
+          );
+        }
+      }
+    }
+
+    return c.json(
+      { error: null, message: "User is successfully created" },
+      201
+    );
+  }
+);
 
 export default register;
--- backend/src/api/users/signin.ts
+++ backend/src/api/users/signin.ts
@@ -1,9 +1,65 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
+
+import prisma from "../../db/prisma";
+import { comparePasswords } from "../../utils/bcrypt";
+import { sign } from "hono/jwt";
 
 const signin = new Hono();
 
-signin.post("", (c) => {
-  return c.json("signing in the user", 200);
+const schema = z.object({
+  email: z.string().trim().email("Email must be valid"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
+
+signin.post(
+  "",
+  zValidator("json", schema, (result, c) => {
+    if (!result.success) {
+      const err = result.error.flatten();
+
+      return c.json({ error: err.fieldErrors }, 400);
+    }
+  }),
+  async (c) => {
+    const payload = await c.req.json();
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email: payload.email },
+      });
+
+      if (!user) {
+        return c.json(
+          { error: { email: "No user found with that email" } },
+          404
+        );
+      }
+
+      const doesPasswordMatch = await comparePasswords(
+        payload.password,
+        user.password
+      );
+
+      if (doesPasswordMatch) {
+        const jwtPayload = {
+          sub: user.id,
+          email: user.email,
+          username: user.username,
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+        };
+
+        const token = await sign(jwtPayload, process.env.JWT_SECRET as string);
+
+        return c.json({ token });
+      } else {
+        return c.json({ error: { password: "Wrong credentials" } }, 404);
+      }
+    } catch (err) {}
+
+    return c.json("signing in the user", 200);
+  }
+);
 
 export default signin;
