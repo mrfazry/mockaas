28e392d3d8dbf8c0f69a4a5642171596047d624d
M. R. Fazry <fazry@theravenry.com>
2024-06-06 21:56:14 +0700

    Add functionality to create film and get list of films

--- backend/src/api/films/index.ts
+++ backend/src/api/films/index.ts
@@ -1,17 +1,46 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 import byId from "./byId";
+import prisma from "../../db/prisma";
 
 const app = new Hono();
 
 app.route("/:id", byId);
 
-app.post("", (c) => {
-  return c.json("create film", 201);
+const createFilmSchema = z.object({
+  title: z.string().trim().min(1, "Title must be filled"),
+  description: z.string().trim().optional(),
+  image_thumbnail: z.string().url("Image thumbnail URL must be valid"),
 });
 
-app.get("", (c) => {
-  return c.json("retrieve list of films", 200);
+app.post(
+  "",
+  zValidator("json", createFilmSchema, (result, c) => {
+    if (!result.success) {
+      const err = result.error.flatten();
+
+      return c.json({ error: err.fieldErrors }, 400);
+    }
+  }),
+  async (c) => {
+    const payload = await c.req.json();
+
+    try {
+      const film = await prisma.film.create({ data: payload });
+
+      return c.json({ data: film, error: null }, 201);
+    } catch (err) {
+      return c.json({ error: "Cannot add film", data: null }, 400);
+    }
+  }
+);
+
+app.get("", async (c) => {
+  const films = await prisma.film.findMany();
+
+  return c.json({ data: films, error: null }, 200);
 });
 
 export default app;
--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
+import { jwt } from "hono/jwt";
 
 import films from "./films";
 import users from "./users";
 
 const app = new Hono();
+
+app.use(
+  "/films/*",
+  jwt({
+    secret: process.env.JWT_SECRET as string,
+  })
+);
 
 app.route("/films", films);
 app.route("/users", users);
