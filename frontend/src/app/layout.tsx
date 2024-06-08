6fc20713b8200e838ab9dbbecb7ed64aec8dc972
M. R. Fazry <fazry@theravenry.com>
2024-06-08 18:55:10 +0700

    Reset global layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-blue-700 min-h-screen`}>
         <Providers>{children}</Providers>
       </body>
     </html>
