d81ed69dc232f03472caf87fca50a413d786ccc4
M. R. Fazry <fazry@theravenry.com>
2024-06-08 17:25:14 +0700

    Add cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 import api from "./api";
 
 const app = new Hono();
+
+app.use("*", cors());
 
 app.route("/api", api);
 
